29ba6d512ef7cbecba2690f8ec3f714e65d27060
FitzEdin <fitzroy.edinborough@gmail.com>
2020-01-30 09:15:37 -0400

    moved network call to Activity class..

--- app/src/main/java/kn/muscovado/scholarships/ItemsFragment.kt
+++ app/src/main/java/kn/muscovado/scholarships/ItemsFragment.kt
@@ -1,33 +1,18 @@
 package kn.muscovado.scholarships
 
 
-import android.content.Context
-import android.net.ConnectivityManager
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.BasicNetwork
-import com.android.volley.toolbox.DiskBasedCache
-import com.android.volley.toolbox.HurlStack
-import com.android.volley.toolbox.JsonArrayRequest
-import kn.muscovado.scholarships.content.Item
-import kotlinx.android.synthetic.main.fragment_items.*
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
 import io.realm.Realm
 import io.realm.kotlin.where
+import kn.muscovado.scholarships.content.Item
+import kotlinx.android.synthetic.main.fragment_items.*
 
 class ItemsFragment : Fragment() {
 
@@ -36,15 +21,8 @@ class ItemsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        val view = inflater.inflate(R.layout.fragment_items, container, false)
-
-        // TODO: Alternatively, query the API for new scholarships, and get the new ones
-        if (isConnected())
-            getItems()
-
         // Inflate the layout for this fragment
-        return view
+        return inflater.inflate(R.layout.fragment_items, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -58,7 +36,6 @@ class ItemsFragment : Fragment() {
         )
         unlock.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_to_loginFrag)
-//            Navigation.createNavigateOnClickListener(R.id.action_to_editItemFrag)
         )
 
         if (listView is RecyclerView) {
@@ -68,101 +45,15 @@ class ItemsFragment : Fragment() {
             }
         }
 
-        if (realm.where<Item>().findAll().size > 0) {
+        if (realm.where<Item>()
+                .equalTo(constants.ITEM_STATUS, constants.STATUS_VETTED)
+                .findAll().size > 0) {
             listView.visibility = View.VISIBLE
             empty_items_list.visibility = View.GONE
         }
         else {
             listView.visibility = View.GONE
             empty_items_list.visibility = View.VISIBLE
-        }
-    }
-
-    private fun isConnected() : Boolean {
-        val cm = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork = cm.activeNetworkInfo
-        return activeNetwork != null && activeNetwork.isConnected
-//        if (!isConnected) {
-//            Toast.makeText(this.context, "No network", Toast.LENGTH_LONG).show()
-//        }
-    }
-
-    private fun getItems() {
-        val url = constants.BASE_URL + constants.PORT + constants.SCHOLARSHIPS
-        val cache = DiskBasedCache(activity?.cacheDir, 1024 * 1024)
-        val network = BasicNetwork(HurlStack(null, null))
-        val requestQueue = RequestQueue(cache, network).apply { start() }
-
-        val jsonArrayRequest = JsonArrayRequest(
-            Request.Method.GET, url, null,
-
-            // handle positive response
-            Response.Listener<JSONArray> { response: JSONArray ->
-                Log.d(constants.LOG_TAG, response.toString())
-
-                // Clear existing Realm
-                realm.beginTransaction()
-                realm.where<Item>().findAll().deleteAllFromRealm()
-                realm.commitTransaction()
-
-                for (i in 0 until response.length()) {
-                    //extract JSON object from response & convert to Kotlin object
-                    val bob = getItem(response.getJSONObject(i))
-                    //and add to list of Items
-                    realm.beginTransaction()
-                    realm.copyToRealm(bob)
-                    realm.commitTransaction()
-
-//                    listView.visibility = View.VISIBLE
-//                    empty_items_list.visibility = View.GONE
-                }
-
-                realm.close()
-            },
-
-            // Log an error response
-            Response.ErrorListener { err ->
-                Log.e(constants.LOG_TAG, err.toString())
-//                listView.visibility = View.GONE
-//                empty_items_list.visibility = View.VISIBLE
-            }
-        )
-
-        requestQueue.add(jsonArrayRequest)
-    }
-
-    // Parse objects from array
-    @NonNull
-    @Throws(JSONException::class)
-    private fun getItem(bob: JSONObject): Item {
-        if (bob.getString(constants.ITEM_STATUS) == constants.STATUS_VETTED) {
-            Log.d("getItem()", bob.getString(constants.ITEM_TITLE))
-
-            return Item(
-                bob.getString(constants.ITEM_ID),
-                bob.getString(constants.ITEM_TITLE),
-                bob.getString(constants.ITEM_COVERAGE),
-                bob.getString(constants.ITEM_LEVEL),
-                bob.getString(constants.ITEM_PROGRAMME),
-                bob.getString(constants.ITEM_LOCATION),
-                bob.getString(constants.ITEM_OPEN_TO),
-                bob.getString(constants.ITEM_LINK),
-                bob.getString(constants.ITEM_STATUS)
-            )
-        }else {
-            Log.d("getItem()", bob.getString(constants.ITEM_ID))
-
-            return Item(
-                bob.getString(constants.ITEM_ID),
-                constants.UNVETTED_ITEM,
-                constants.UNVETTED_ITEM,
-                constants.UNVETTED_ITEM,
-                constants.UNVETTED_ITEM,
-                constants.UNVETTED_ITEM,
-                constants.UNVETTED_ITEM,
-                bob.getString(constants.ITEM_LINK),
-                bob.getString(constants.ITEM_STATUS)
-            )
         }
     }
 }
--- app/src/main/java/kn/muscovado/scholarships/MainActivity.kt
+++ app/src/main/java/kn/muscovado/scholarships/MainActivity.kt
@@ -8,16 +8,35 @@ import android.net.ConnectivityManager
 import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Toast
+import androidx.annotation.NonNull
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
+import com.android.volley.toolbox.JsonArrayRequest
+import io.realm.Realm
+import io.realm.kotlin.where
+import kn.muscovado.scholarships.content.Item
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    var rcvr : BroadcastReceiver = makeBroadcastReceiver()
+    private var rcvr : BroadcastReceiver = makeBroadcastReceiver()
+    private var realm = Realm.getDefaultInstance()
+    private val constants = Constants()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (isConnected()) {    getItems()    }
     }
 
     override fun onStart() {
@@ -40,25 +59,11 @@ class MainActivity : AppCompatActivity() {
     private fun makeBroadcastReceiver() : BroadcastReceiver {
         return object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
-//                val networkStateExtra = intent.getBooleanExtra(
-//                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, true
-//                )
 
                 val msg = when (isConnected()) {
                     true -> "Online"
                     else -> "No network"
                 }
-//                val wifiStateExtra = intent.getIntExtra(
-//                    WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN)
-//
-//                when (wifiStateExtra) {
-//                    WifiManager.WIFI_STATE_ENABLED,
-//                    WifiManager.WIFI_STATE_ENABLING
-//                        -> msg = "Wifi on"
-//                    WifiManager.WIFI_STATE_DISABLED,
-//                    WifiManager.WIFI_STATE_DISABLING
-//                        -> msg = "Wifi off"
-//                }
 
                 val t = Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG)
                 t.setGravity(Gravity.CENTER, 0, 0)
@@ -66,5 +71,79 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun getItems() {
+        val url = constants.BASE_URL + constants.PORT + constants.SCHOLARSHIPS
+        val cache = DiskBasedCache(cacheDir, 1024 * 1024)
+        val network = BasicNetwork(HurlStack(null, null))
+        val requestQueue = RequestQueue(cache, network).apply { start() }
+
+        val jsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET, url, null,
+
+            // handle positive response
+            Response.Listener<JSONArray> { response: JSONArray ->
+                Log.d(constants.LOG_TAG, response.toString())
+
+                // Clear existing Realm
+                realm.beginTransaction()
+                realm.where<Item>().findAll().deleteAllFromRealm()
+                realm.commitTransaction()
+
+                for (i in 0 until response.length()) {
+                    //extract JSON object from response & convert to Kotlin object
+                    val bob = getItem(response.getJSONObject(i))
+                    //and add to list of Items
+                    realm.beginTransaction()
+                    realm.copyToRealm(bob)
+                    realm.commitTransaction()
+                }
+
+                realm.close()
+            },
+
+            // Log an error response
+            Response.ErrorListener { err ->
+                Log.e(constants.LOG_TAG, err.toString())
+            }
+        )
+
+        requestQueue.add(jsonArrayRequest)
+    }
+
+    // Parse objects from array
+    @NonNull
+    @Throws(JSONException::class)
+    private fun getItem(bob: JSONObject): Item {
+        if (bob.getString(constants.ITEM_STATUS) == constants.STATUS_VETTED) {
+            Log.d("getItem()", bob.getString(constants.ITEM_TITLE))
+
+            return Item(
+                bob.getString(constants.ITEM_ID),
+                bob.getString(constants.ITEM_TITLE),
+                bob.getString(constants.ITEM_COVERAGE),
+                bob.getString(constants.ITEM_LEVEL),
+                bob.getString(constants.ITEM_PROGRAMME),
+                bob.getString(constants.ITEM_LOCATION),
+                bob.getString(constants.ITEM_OPEN_TO),
+                bob.getString(constants.ITEM_LINK),
+                bob.getString(constants.ITEM_STATUS)
+            )
+        }else {
+            Log.d("getItem()", bob.getString(constants.ITEM_ID))
+
+            return Item(
+                bob.getString(constants.ITEM_ID),
+                constants.UNVETTED_ITEM,
+                constants.UNVETTED_ITEM,
+                constants.UNVETTED_ITEM,
+                constants.UNVETTED_ITEM,
+                constants.UNVETTED_ITEM,
+                constants.UNVETTED_ITEM,
+                bob.getString(constants.ITEM_LINK),
+                bob.getString(constants.ITEM_STATUS)
+            )
+        }
     }
 }
