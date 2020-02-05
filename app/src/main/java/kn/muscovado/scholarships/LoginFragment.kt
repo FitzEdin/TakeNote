0d401fe22c0cb6e21aedd5552a6c872a74094a99
FitzEdin <fitzroy.edinborough@gmail.com>
2020-02-05 10:53:55 -0400

    checking validity of PIN entered when logging in..

--- app/src/main/java/kn/muscovado/scholarships/LoginFragment.kt
+++ app/src/main/java/kn/muscovado/scholarships/LoginFragment.kt
@@ -2,11 +2,14 @@ package kn.muscovado.scholarships
 
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
@@ -29,9 +32,22 @@ class LoginFragment : Fragment() {
         )
 
         // to edit item
-        login_btn.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_to_editListFrag)
-        )
+        login_btn.setOnClickListener{login()}
+    }
+
+    private fun login(){
+        // get PIN entered
+        val pin = login_text.text.toString()
+        // check validity
+        if (pin == "1869") {
+            //      go to editListFrag
+            this.findNavController().navigate(R.id.action_to_editListFrag)
+        } else {
+            //      else let user know
+            val tEmpty = Toast.makeText(this.requireContext(), "Bob dropped the ball", Toast.LENGTH_LONG)
+            tEmpty.setGravity(Gravity.CENTER, 0,0)
+            tEmpty.show()
+        }
     }
 
 
--- app/src/main/java/kn/muscovado/scholarships/SearchItemsFragment.kt
+++ app/src/main/java/kn/muscovado/scholarships/SearchItemsFragment.kt
@@ -1,7 +1,6 @@
 package kn.muscovado.scholarships
 
-import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
